3dc9ad640a1781038de6d5c82be8f0aecf9022a2
Pickering <theoromeo@gmail.com>
2023-08-09 16:46:55 +0200

    test: add jest and test script

--- Clients/javascript/src/index.js
+++ Clients/javascript/src/index.js
@@ -1,4 +1,33 @@
 class NextPage
 {
-    
+    /**
+     * Gets Webpage for parsing.
+     * 
+     * @param {URL} url - URL of webpage to retrieve.
+     * @returns {(String|Number)} 
+     * - HTML if webpage retrieved successfully, 
+     * - -1 if fetch error, 
+     * - -2 if connection error.
+     */
+    async getWebpage(url , key)
+    {
+        let webpageResponse = null;
+
+        try 
+        {
+            const request = await fetch(url)
+
+            if(!request.ok)
+            {
+                return -2;
+            }
+
+            webpageResponse = await request.text()
+        }
+        catch(error)
+        {
+
+        }
+
+    }
 }
