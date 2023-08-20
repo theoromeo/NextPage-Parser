980b0ab19481ee7776c39744ec0210649b122a7b
Pickering <theoromeo@gmail.com>
2023-08-20 07:29:03 +0200

    test: add temp tests.

--- Clients/javascript/tests/index.test.js
+++ Clients/javascript/tests/index.test.js
@@ -1,11 +1,50 @@
 import NextPage from "../src/index.js"
+import { DOMParser , XMLSerializer } from 'xmldom-qsa'
 
-it("Init Test",async () => {
+it("Fetch Page" , async () => 
+{
+    const NP = new NextPage()
+    const page = await NP.getWebpage("fetch.html")
+    expect(page).toEqual("true")
 
-    let x = new NextPage();
-    let r = await x.getWebpage("johnmayer.html","fender");
-
-
-    expect(r).toEqual("Johnmayer HTML")
-
+    const page2 = await NP.getWebpage("non_existent_page.html")
+    expect(page2).toEqual(-2)
 })
+
+it("Dom Conversion" , async () => 
+{
+    let isNumber = true
+    const NP = new NextPage()
+    const page = await NP.getWebpage("johnmayer.html")
+    const dom = await NP.toDOM(page)
+
+    expect(dom.childNodes).toBeDefined()
+})
+
+it("Node retrieval" , async () => 
+{
+    let isNumber = true
+    const NP = new NextPage()
+    const page = await NP.getWebpage("johnmayer.html")
+    const dom = await NP.toDOM(page)
+    const node = NP.getNode(dom,"fender")
+
+    expect(node.toString()).toContain("np-for")
+})
+
+it("Get View Information" , async () => 
+{
+    let isNumber = true
+    const NP = new NextPage()
+    const page = await NP.getWebpage("johnmayer.html")
+    const dom = await NP.toDOM(page)
+    const node = NP.getNode(dom,"PSG")
+
+    const info = NP.getViewInformation(node)
+
+    expect(info.query).toContain("img:nth-child(-n+6)")
+    expect(info.type).toContain("image.grid")
+})
+
+
+
