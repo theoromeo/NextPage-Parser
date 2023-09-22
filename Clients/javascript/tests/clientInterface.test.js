cda25d0e11c88be55eb27c764237daabe554fee0
Pickering <theoromeo@gmail.com>
2023-09-22 14:41:23 +0200

    test: core client interfaces

---
+++ Clients/javascript/tests/clientInterface.test.js
@@ -0,0 +1,70 @@
+import NextPage from "../src/index.js"
+
+
+const NP = new NextPage()
+const PAGE = await NP.getWebpage("www.html")
+const DOM = NP.toDOM(PAGE)
+
+// Operations clients are must likely to use the most.
+describe.skip("Client Interfaces", () => 
+{
+    it("Get Node Result ", async ()=>
+    {
+        const data = await NP.get("www.html","for-basic")
+
+        expect(data).toBeDefined();
+    })
+
+
+    it("Get Header Properties ", async ()=>
+    {
+        const data = await NP.getHeaderProperties(DOM)
+
+        expect(data.title).toBeDefined();
+        expect(data.description).toBeDefined();
+        expect(data.icon).toEqual(false);
+    })
+
+    it("Get Appended Header Properties", async ()=>
+    {
+        const data = await NP.get("www.html","for-article")
+        
+        expect(data.result).toBeDefined();
+        expect(data.description).toBeDefined();
+        expect(data.title).toBeDefined();
+
+    })
+
+    it("Get Node Defined Header Properties", async ()=>
+    {
+        const data = await NP.get("www.html","for-article-headers")
+        
+        expect(data.description).toContain("#for-article-headers");
+        expect(data.title).toContain("#for-article-headers");
+
+    })
+
+
+    it("Handel Missing Node", async ()=>
+    {
+        const data =await NP.get("www.html","for-non-exist")
+        
+        expect(data).toEqual(-2);
+    })
+
+
+    it("Handel Missing Header Properties", async ()=>
+    {
+        const NP = new NextPage()
+        const PAGE = await NP.getWebpage("missingHeaders.html")
+        const DOM = NP.toDOM(PAGE)
+
+        const data =await NP.getHeaderProperties(DOM)
+        
+        expect(data.title).toContain("Document Title");
+        expect(data.description).toEqual(false);
+        expect(data.icon).toEqual(false);
+    })
+})
+
+
